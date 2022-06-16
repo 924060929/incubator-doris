47c30d5c51bcae5cc5004d4fac54b649d7d4bc61
Shuo Wang <wangshuo128@gmail.com>
2022-06-16 14:32:20 +0800

    bind

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/AbstractPlan.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/plans/AbstractPlan.java
@@ -92,4 +92,9 @@ public abstract class AbstractPlan<OP_TYPE extends PlanOperator>
             treeString(lines, depth + 1, newLasts, children.get(i));
         }
     }
+
+    @Override
+    public String toString() {
+        return operator.toString();
+    }
 }
