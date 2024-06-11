4e668fb70addd28f9026598885cbe5895225e749
924060929 <924060929@qq.com>
2024-06-11 11:05:25 +0800

    save

--- fe/fe-core/src/main/java/org/apache/doris/nereids/worker/job/ScanRanges.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/worker/job/ScanRanges.java
@@ -83,7 +83,7 @@ public class ScanRanges implements Splittable<ScanRanges> {
 
     /** toString */
     public void toString(StringBuilder str, String prefix) {
-        str.append("ScanRanges(bytes:" + totalBytes + ", ranges: [\n");
+        str.append("ScanRanges(bytes: " + totalBytes + ", ranges: [\n");
         for (int i = 0; i < params.size(); i++) {
             str.append(prefix).append("  " + toString(params.get(i)) + ", bytes: " + bytes.get(i));
             if (i + 1 < params.size()) {
