ec4ea37e81ae2ee3960b7db46798185f8882fb2f
924060929 <924060929@qq.com>
2024-06-12 15:16:46 +0800

    save

--- fe/fe-core/src/main/java/org/apache/doris/nereids/worker/job/AbstractUnassignedScanJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/worker/job/AbstractUnassignedScanJob.java
@@ -83,7 +83,7 @@ public abstract class AbstractUnassignedScanJob extends AbstractUnassignedJob {
                         scanNodes, 1 // don't split scan ranges
                 );
 
-                // Some tablets too big, we need add parallel to process this tablet,
+                // Some tablets too big, we need add parallel to process this tablet after scan,
                 // for example, use one OlapScanNode to scan data, and use some instances
                 // to process Aggregation parallel. we call this `share scan`. Backend will
                 // know this instances share the same ScanSource, and will not scan same data
