50ac1fe643350da4b8645d2061825faa11743295
924060929 <924060929@qq.com>
2024-09-23 16:14:30 +0800

    join LoadProcessor

--- fe/fe-core/src/main/java/org/apache/doris/qe/LoadContext.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/LoadContext.java
@@ -31,6 +31,7 @@ import java.util.List;
 import java.util.Map;
 
 public class LoadContext {
+
     private final Map<String, String> loadCounters = Maps.newLinkedHashMap();
     private final List<String> deltaUrls = Lists.newCopyOnWriteArrayList();
 
